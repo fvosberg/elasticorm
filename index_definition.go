d281610dd826492d2a6a66d5a8443b4a8c9f5c99
Frederik Vosberg <frederik.vosberg@diemedialen.de>
2017-02-18 01:13:46 +0100

    [FEATURE] index definition with number of shards

--- index_definition.go
+++ index_definition.go
@@ -1,8 +1,28 @@
 package elasticorm
 
 type IndexDefinition struct {
+	Settings *indexSettings `json:"settings,omitempty"`
 }
 
-func NewIndexDefinition(indexName string) IndexDefinition {
-	return IndexDefinition{}
+type indexSettings struct {
+	NumberOfShards int `json:"number_of_shards"`
+}
+
+type IndexDefinitionFunc func(*IndexDefinition)
+
+func NewIndexDefinition(options ...IndexDefinitionFunc) IndexDefinition {
+	def := IndexDefinition{}
+	for _, opt := range options {
+		opt(&def)
+	}
+	return def
+}
+
+func SetNumberOfShards(number int) IndexDefinitionFunc {
+	return func(def *IndexDefinition) {
+		if def.Settings == nil {
+			def.Settings = &indexSettings{}
+		}
+		def.Settings.NumberOfShards = number
+	}
 }
--- index_definition_test.go
+++ index_definition_test.go
@@ -8,9 +8,30 @@ import (
 )
 
 func TestIndexDefinition(t *testing.T) {
-	def := elasticorm.NewIndexDefinition(`Customers`)
+	tests := []struct {
+		title        string
+		def          elasticorm.IndexDefinition
+		expectedJSON string
+	}{
+		{
+			title:        `Empty Index definition`,
+			def:          elasticorm.NewIndexDefinition(),
+			expectedJSON: `{}`,
+		},
+		{
+			title: `Index definition with number of shards setting`,
+			def: elasticorm.NewIndexDefinition(
+				elasticorm.SetNumberOfShards(3),
+			),
+			expectedJSON: `{"settings":{"number_of_shards":3}}`,
+		},
+	}
 
-	actualJSON, err := json.Marshal(def)
-	ok(t, err)
-	equals(t, `{}`, string(actualJSON))
+	for _, tt := range tests {
+		t.Run(tt.title, func(t *testing.T) {
+			actualJSON, err := json.Marshal(tt.def)
+			ok(t, err)
+			equals(t, tt.expectedJSON, string(actualJSON))
+		})
+	}
 }
